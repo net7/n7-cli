2e2c4920c1d1097f1b32a887fcd12c3ad4d4c97e
Edgar Gomez <gomez@netseven.it>
2020-03-03 13:11:28 +0100

    removed unused try catch

--- cli/n7-cli.js
+++ cli/n7-cli.js
@@ -4,38 +4,32 @@ const program = require('commander');
 const CommandNew = require('./commands/new');
 const CommandLayout = require('./commands/layout');
 
-try {
-  // DESCRIPTION
-  // ---------------------------------------------------------->
-  program
-    .version('0.0.1')
-    .description('A command line interface for n7-framework');
-  
-  // NEW
-  // ---------------------------------------------------------->
-  program
-    .command('new <name>')
-    .alias('n')
-    .description('creates a new n7-cli app')
-    .option('-v, --verbose', 'output extra info')
-    .option('-t, --type <type>', 'app type < arianna | muruca | dataviz | empty >', 'empty')
-    .option('-p, --prefix <prefix>', 'app prefix', 'app')
-    .action((name, { verbose, type, prefix }) => new CommandNew(name, type, prefix, !!verbose));
-  
-  // LAYOUT
-  // ---------------------------------------------------------->
-  program
-    .command('layout <name>')
-    .alias('l')
-    .description('adds a new layout')
-    .option('-v, --verbose', 'output extra info')
-    .option('-D, --no-datasource', 'without layout datasource')
-    .option('-E, --no-eventhandler', 'without layout eventhandler')
-    .action((name, { verbose, datasource, eventhandler }) => new CommandLayout(name, datasource, eventhandler, !!verbose));
-  
-  program.parse(process.argv);
-  
-} catch(err) {
-  console.error(err);
-  process.exit(1);
-};
+// DESCRIPTION
+// ---------------------------------------------------------->
+program
+  .version('0.0.1')
+  .description('A command line interface for n7-framework');
+
+// NEW
+// ---------------------------------------------------------->
+program
+  .command('new <name>')
+  .alias('n')
+  .description('creates a new n7-cli app')
+  .option('-v, --verbose', 'output extra info')
+  .option('-t, --type <type>', 'app type < arianna | muruca | dataviz | empty >', 'empty')
+  .option('-p, --prefix <prefix>', 'app prefix', 'app')
+  .action((name, { verbose, type, prefix }) => new CommandNew(name, type, prefix, !!verbose));
+
+// LAYOUT
+// ---------------------------------------------------------->
+program
+  .command('layout <name>')
+  .alias('l')
+  .description('adds a new layout')
+  .option('-v, --verbose', 'output extra info')
+  .option('-D, --no-datasource', 'without layout datasource')
+  .option('-E, --no-eventhandler', 'without layout eventhandler')
+  .action((name, { verbose, datasource, eventhandler }) => new CommandLayout(name, datasource, eventhandler, !!verbose));
+
+program.parse(process.argv);
