b6512c3779b105879f1cae4ef6d5744e17dd13c1
Edgar Gomez <gomez@netseven.it>
2020-11-11 12:49:03 +0100

    added command handler

---
+++ cli/commands/sls.js
@@ -0,0 +1,190 @@
+const fs = require('fs-extra');
+const path = require('path');
+const replace = require('replace-in-file');
+const helpers = require('./helpers');
+
+const nameRegexPattern = /[a-z]/;
+const nameMinLimit = 3;
+const nameMaxLimit = 20;
+const filesToReplace = [
+  'src/parsers/baseslsname-home.ts',
+  'src/parsers/baseslsname-resources.ts',
+  'src/parsers/baseslsname-search.ts',
+];
+const placeholdersToReplace = {
+  pointer: ['name', 'name', 'prefix'],
+  from: [/baseslsname/g, /BASESLSNAME/g, /Baseslsname/g],
+  files: [
+    'package.json',
+    'README.md',
+    'src/parsers/baseslsname-home.ts',
+    'src/parsers/baseslsname-resources.ts',
+    'src/parsers/baseslsname-search.ts',
+    'src/parsers/index.ts',
+  ]
+};
+const additionalFilesToRemove = [
+  'gitignore.tpl.txt',
+];
+
+
+class CommandNew {
+  constructor(name, verbose) {
+    this.name = name;
+    this.verbose = verbose;
+    this.targetPath = `${process.cwd()}/${this.name}-sls`;
+    this.srcPath = path.join(path.dirname(fs.realpathSync(__filename)), '../../base-sls');
+
+    // validation
+    this.validate();
+
+    // init
+    helpers.log('loading files...');
+    this.directoryExists()
+      .then((exists) => {
+        if(exists){
+          helpers.error(`directory already exists ${this.targetPath}`);
+        } 
+        return this.copy();
+      })
+      .then(() => {
+        helpers.log(`loading .gitignore...`);
+        return this.copyGitignore();
+      })
+      .then(() => {
+        helpers.log(`setup ${this.type} environment...`);
+        return this.replaceFiles();
+      })
+      .then(() => {
+        helpers.log(`cleaning up unused files...`);
+        return this.cleanUpFiles();
+      })
+      .then(() => {
+        helpers.log(`setting app name and prefix...`);
+        return this.replacePlaceholders();
+      })
+      .then(() => {
+        helpers.log(`new n7 app created! path: ${this.targetPath}`);
+      })
+      .catch(reason => {
+        helpers.error(reason);
+      });
+  }
+
+  validate(){
+    // validate name characters
+    if(!nameRegexPattern.test(this.name)){
+      helpers.error(`name ${this.name} format error - allowed chars [a-z]`);
+    }
+    
+    // validate name length
+    if((this.name.length > nameMaxLimit) || (this.name.length < nameMinLimit)){
+      helpers.error(`name ${this.name} length error - must be ${nameMinLimit} to ${nameMaxLimit} chars`);
+    }
+  }
+
+  directoryExists() {
+    // info...
+    this.printInfo(`directory exists control: ${this.targetPath}`);
+    
+    return fs.pathExists(this.targetPath).catch((err) => {
+      console.log('directory exists fail', err);
+      throw new Error('directory exists fail');
+    });
+  }
+
+  copy() {
+    // info...
+    this.printInfo(`copying files from ${this.srcPath} to ${this.targetPath}`);
+    
+    return fs.copy(this.srcPath, this.targetPath).catch((err) => {
+      console.log('copy fail', err);
+      throw new Error('copy fail');
+    });
+  }
+
+  copyGitignore() {
+    const src = this.srcPath + '/gitignore.tpl.txt';
+    const target = this.targetPath + '/.gitignore';
+
+    // info...
+    this.printInfo(`copying gitignore from ${src} to ${target}`);
+    
+    return fs.copy(src, target).catch((err) => {
+      console.log('copy .gitignore fail', err);
+      throw new Error('copy .gitignore fail');
+    });
+  }
+
+  replaceFiles() {
+    // type files
+    const files = [];
+    filesToReplace.forEach((file) => {
+      const src = file;
+      const dest = file.replace('baseslsname', this.name);
+      files.push({ src, dest })
+    });
+
+    // info...
+    this.printInfo([
+      'replacing environment files',
+      ...files.map(({ src, dest }) => `- ${this.name}-sls/${src} => ${this.name}-sls/${dest}`)
+    ].join('\n'));
+    
+    return Promise.all(
+      files.map(({ src, dest }) => fs.copy(`${this.targetPath}/${src}`, `${this.targetPath}/${dest}`))
+    ).catch((err) => {
+      console.log('replace files fail', err);
+      throw new Error('replace files fail');
+    });
+  }
+
+  cleanUpFiles() {
+    const files = [];
+
+    // additional files to remove
+    additionalFilesToRemove.forEach(file => {
+      files.push(file);
+    });
+
+    // info...
+    this.printInfo([
+      'removing unused files',
+      ...files.map((file) => `- ${this.name}-sls/${file}`)
+    ].join('\n'));
+
+    return Promise.all(
+      files.map(file => fs.remove(`${this.targetPath}/${file}`))
+    ).catch((err) => {
+      console.log('clean up fail', err);
+      throw new Error('clean up fail');
+    });
+  }
+
+  replacePlaceholders() {
+    const { pointer, from, files } = placeholdersToReplace;
+
+    // info...
+    this.printInfo([
+      'replacing project placeholders (name) in: ',
+      ...files.map((file) => `- ${this.name}-sls/${file}`)
+    ].join('\n'));
+
+    return replace({
+      from,
+      to: pointer.map(point => this[point]),
+      files: files.map(file => `${this.targetPath}/${file}`),
+    }).catch((err) => {
+      console.log('replace placeholders fail', err);
+      throw new Error('replace placeholders fail');
+    });
+  }
+
+  printInfo(msg){
+    if(this.verbose){
+      helpers.info(msg);
+    }
+  }
+}
+
+module.exports = CommandNew;
