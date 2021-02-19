7c912a9f8ebd9a8762207fbe8a2550f7f13025a7
Edgar Gomez <gomez@netseven.it>
2021-02-19 15:56:18 +0100

    updated sls files

--- base-sls/src/controller.ts
+++ base-sls/src/controller.ts
@@ -1,14 +1,14 @@
 import { Controller } from '@n7-frontend/serverless';
-import parsers from "./parsers";
-import configurations from "./configurations";
+import parsers from './parsers';
+import configurations from './configurations';
 
 const controller = new Controller({
   parsers,
   configurations,
   baseUrl: process.env.BASE_URL,
+  staticUrl: process.env.STATIC_URL,
   searchIndex: process.env.SEARCH_INDEX,
-  elasticUri: process.env.ELASTIC_URI,
-  pages: process.env.PAGES
+  elasticUri: process.env.ELASTIC_URI
 });
 
 const methods = controller.getSlsMethods();
@@ -16,7 +16,10 @@ const methods = controller.getSlsMethods();
 export const getNavigation = methods.getNavigation;
 export const getFooter = methods.getFooter;
 export const getHomeLayout = methods.getHomeLayout;
+export const getSearchDescription = methods.getSearchDescription;
+export const getTimeline = methods.getTimeline;
 export const getResource = methods.getResource;
 export const search = methods.search;
 export const getTranslation = methods.getTranslation;
 export const getStaticPage = methods.getStaticPage;
+export const getStaticPost = methods.getStaticPost;
--- base-sls/src/parsers/baseslsname-home.ts
+++ base-sls/src/parsers/baseslsname-home.ts
@@ -1,6 +1,22 @@
 import { HomeParser } from '@n7-frontend/serverless';
 
 export class BaseslsnameHomeParser extends HomeParser {
+  protected parseSliderItems(data: any, block: string) {
+    if (/slider-main/.test(block)) {
+      return data.map((d: any) => ({
+        items: [{ title: d.title }, { text: d.text }],
+        action: {
+          text: d.button.text,
+          anchor: {
+            href: d.button.anchor,
+          },
+        },
+        background: {
+          image: d.image,
+        },
+      }));
+    }
+  }
   protected parseCollectionItems(data: any, block: string) {
     if (/collection-works/.test(block)) {
       return data.items.map((d: any) => ({
