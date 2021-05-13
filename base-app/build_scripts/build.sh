e56e0ee896de5811167d6158c1510d74e0188f1e
Edgar Gomez <gomez@netseven.it>
2021-05-13 12:05:34 +0200

    fix scripts

--- base-app/build_scripts/build.sh
+++ base-app/build_scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-BUILD_CMD="npm install --force && npm run build -- --prod"
+BUILD_CMD="export NG_CLI_ANALYTICS=\"false\" && npm install --force && npm run build -- --prod"
 
 docker run -it -u 0 --rm --name ngcompile -v "$PWD"/..:/ng -w /ng node:14.15.5-stretch sh -c "$BUILD_CMD"
