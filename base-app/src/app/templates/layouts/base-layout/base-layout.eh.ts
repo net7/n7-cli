d5aaa4c58c9e2f1f240717eba1f4de691f5ad4f2
Edgar Gomez <gomez@netseven.it>
2022-10-13 16:04:05 +0200

    added layout dataSource property

--- base-app/src/app/templates/layouts/base-layout/base-layout.eh.ts
+++ base-app/src/app/templates/layouts/base-layout/base-layout.eh.ts
@@ -1,6 +1,9 @@
 import { EventHandler } from '@net7/core';
+import { BaseLayoutDS } from './base-layout.ds';
 
 export class BaseLayoutEH extends EventHandler {
+  dataSource: BaseLayoutDS;
+
   public listen() {
     this.innerEvents$.subscribe(({ type, payload }) => {
       switch (type) {
