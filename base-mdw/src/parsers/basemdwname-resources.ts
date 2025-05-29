1a89f4d844511d178bfc8135429babc23b141f52
Luca Poggianti <apoggianti.luca@gmail.com>
2025-05-29 17:19:42 +0200

    Clean examples config

--- base-mdw/src/parsers/basemdwname-resources.ts
+++ base-mdw/src/parsers/basemdwname-resources.ts
@@ -2,65 +2,48 @@ import { ParsedData } from '@n7-frontend/n7-muruca-middleware';
 import { ResourceParser } from '@n7-frontend/n7-muruca-middleware/dist/parsers/resource';
 
 export class BasemdwnameResourceParser extends ResourceParser {
-  parse({ data, options }: any) {
-    if (!('type' in options)) {
-      return;
-    }
+ 
 
-    const { conf, type } = options;
+  // PARSERS
+  // parseCollection(block: ConfBlock, data: any): OutputCollection {
+  //   const collection : OutputCollection = {
+  //     items: [],
+  //   };
+  //   block.fields.map((field: string) => {
+  //     if (data[field]) {
+  //       if (field === 'collegamenti_interni') {
+  //         collection.items = data[field].map((item) => {
+  //           const collectionItem = {
+  //             title: item.collegamento[0].title,
+  //             slug: item.collegamento[0].slug,
+  //             id: item.collegamento[0].id,
+  //             routeId: item.collegamento[0]['record-type'],
+  //             text: item.collegamento_note
+  //           };
+  //           return collectionItem;
+  //         });
+  //       } else if (field === 'collegamenti_esterni') {
+  //         collection.items.push({
+  //           title: data[field].title,
+  //           link: data[field].url,
+  //           slug: '',
+  //           id: 0,
+  //           routeId: ''
+  //         });
+  //       }
+  //     }
+  //   });
+  //   return collection;
+  // }
 
-    const parsed: ParsedData = {
-      title: '',
-      sections: {},
-    };
-
-    for (const block in conf) {
-      switch (block) {
-        case 'header':
-          parsed.sections[block] = {};
-          let t = conf[block].fields;
-          parsed.sections[block][t[0]] = data[t[0]]; // title
-          break;
-
-        case 'metadata':
-          parsed.sections[block] = {};
-
-          const m = {
-            group: [
-              {
-                title: 'Metadata',
-                items: conf[block].fields.map((field: string) => {
-                  if (data[field]) {
-                    const filter = [];
-                    if (filter.indexOf(field) > -1) {
-                      return this.filter(data, field, type); // metadata
-                    } else {
-                      return {
-                        label: field.replace(/_/g, ' '),
-                        value: data[field],
-                      };
-                    }
-                  }
-                }),
-              },
-            ],
-          };
-          m.group[0].items = m.group[0].items.filter((n: any) => n);
-          parsed.sections[block] = { ...m };
-          break;
-
-        default:
-          break;
-      }
-    }
-    return parsed;
-  }
-
-  /**
-   * Data filters
-   */
-  filter(data: any, field: string, page) {
-    let filter;
-    return filter;
-  }
+  // FILTERS
+  // filterMetadataItem(field: string, metadataItem: OutputMetadataItem, recordType: string, data: any): OutputMetadataItem {
+  //   if (/label/.test(field)) {
+  //     metadataItem = {
+  //       label: field.replace(/_/g, " "),
+  //       value: data[field].title
+  //     }
+  //   }
+  //   return metadataItem;
+  // }
 }
