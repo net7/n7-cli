98940de03345f2f883e702f7fa935c12f1e2b78c
Luca Poggianti <apoggianti.luca@gmail.com>
2025-06-24 12:42:04 +0200

    Add styles templates

--- cli/commands/new.js
+++ cli/commands/new.js
@@ -14,6 +14,10 @@ const filesToReplace = [
     ext: "scss",
   },
   {
+    path: "src/styles/components/_header",
+    ext: "scss",
+  },
+  {
     path: "src/app/app.routes",
     ext: "ts",
   },
